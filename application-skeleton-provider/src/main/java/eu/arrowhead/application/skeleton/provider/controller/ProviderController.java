e7cdc8abaee17fcdd7c5d25d41623e3cab806bd3
fred321f <frederick01nielsen@gmail.com>
2024-08-21 16:01:11 +0200

    customization of project and added init method in ProviderController

--- application-skeleton-provider/src/main/java/eu/arrowhead/application/skeleton/provider/controller/ProviderController.java
+++ application-skeleton-provider/src/main/java/eu/arrowhead/application/skeleton/provider/controller/ProviderController.java
@@ -1,9 +1,16 @@
 package eu.arrowhead.application.skeleton.provider.controller;
 
+import ai.aitia.arrowhead.application.library.ArrowheadService;
+import eu.arrowhead.common.dto.shared.ServiceRegistryRequestDTO;
+import eu.arrowhead.common.dto.shared.SystemRequestDTO;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import eu.arrowhead.common.CommonConstants;
+
+import javax.annotation.PostConstruct;
+import java.util.Collections;
 
 @RestController
 public class ProviderController {
@@ -12,6 +19,9 @@ public class ProviderController {
 	// members
 
 	//TODO: add your variables here
+	@Autowired
+	private ArrowheadService arrowheadService;
+
 
 	//=================================================================================================
 	// methods
@@ -21,7 +31,31 @@ public class ProviderController {
 	public String echoService() {
 		return "Got it!";
 	}
-	
+
+	@PostConstruct
+	public void init() {
+		ServiceRegistryRequestDTO serviceRegistryRequest = new ServiceRegistryRequestDTO();
+		serviceRegistryRequest.setServiceDefinition("lenovo_service");
+		serviceRegistryRequest.setProviderSystem(new SystemRequestDTO(
+				"audiocura_service_provider",
+				"localhost",
+				8443,
+				"audiocura_service_provider",
+				null
+        ));
+
+		serviceRegistryRequest.setServiceUri("/simple-service");
+		serviceRegistryRequest.setSecure(null);
+		serviceRegistryRequest.setInterfaces(Collections.singletonList("HTTP-SECURE-JSON"));
+
+		arrowheadService.forceRegisterServiceToServiceRegistry(serviceRegistryRequest);
+	}
+
+	@GetMapping
+	public String provideService() {
+		return "Hello from Lenovo Laptop!";
+	}
+
 	//-------------------------------------------------------------------------------------------------
 	//TODO: implement here your provider related REST end points
 }
